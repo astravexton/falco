5747e2e6e24aa5e106ce09ff544586daf3e6045a
nathan <squ1dge08@gmail.com>
2016-01-27 19:20:54 +0000

    moved repl to own file

---
+++ plugins/repl.py
@@ -0,0 +1,62 @@
+from utils import isAdmin, add_cmd, add_regex
+import code, sys
+
+def main(irc):
+    irc.repl = ""
+    irc.repls = Repl(irc)
+    irc.multirepl = False
+
+class Repl(code.InteractiveConsole):
+    def __init__(self, irc):
+        code.InteractiveConsole.__init__(self, {
+                "falco": irc,
+                "globals": globals})
+        self.irc = irc
+        self.channel = None
+        self.buf = ""
+
+    def write(self, data):
+        self.buf += data
+
+    def flush(self):
+        msg = self.buf.rstrip("\n")
+        if len(msg) > 0:
+            self.irc.msg(self.channel, msg)
+        self.buf = ""
+
+    def run(self, channel, code):
+        self.channel = channel
+        sys.stdout = self
+        self.push(code)
+        sys.stdout = sys.__stdout__
+        self.flush()
+
+    def showtraceback(self):
+        type, value, lasttb = sys.exc_info()
+        self.irc.msg(self.channel, "{0}: {1}".format(type.__name__, value))
+
+    def showsyntaxerror(self, filename):
+        self.showtraceback()
+
+def repl(irc, source, msgtarget, args):
+    if isAdmin(irc, source):
+        irc.repls.run(msgtarget, args)
+
+add_cmd(repl, ">>")
+
+def multirepl(irc, source, msgtarget, args):
+    args = args[0]
+    if isAdmin(irc, source) and irc.multirepl == True and args != '"""':
+        irc.repl+=args+"\n"
+        
+add_regex(multirepl, "(.*)")
+
+def multireplprefix(irc, source, msgtarget, args):
+    if irc.multirepl == True:
+        irc.multirepl = False
+        irc.repls.run(msgtarget, irc.repl)
+        irc.repl = ""
+    else:
+        irc.multirepl = True
+
+add_regex(multireplprefix, '"""')
