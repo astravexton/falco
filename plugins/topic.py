86e1faffc389e1da29c793173b18fa58d53be4d2
nathan <squ1dge08@gmail.com>
2016-02-01 01:58:47 +0000

    working topic command (really this time (i really do mean it))

--- plugins/topic.py
+++ plugins/topic.py
@@ -4,7 +4,7 @@ from utils import *
 def topic(irc, source, msgtarget, args):
     topics = irc.channels[msgtarget]["topic"].split(" | ")
     while True:
-        t = "new topic | {}".format(topic)
+        t = "{} | {}".format(args, irc.channels[msgtarget]["topic"])
         if len(t) > 307:
             t = t.split(" | ")[0:-1]
         else:
