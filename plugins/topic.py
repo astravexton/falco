10d5cd524d174853b5095dde4f24e9e5097603aa
nathan <squ1dge08@gmail.com>
2016-02-01 03:13:58 +0000

    working topic (please)

--- plugins/topic.py
+++ plugins/topic.py
@@ -3,11 +3,11 @@ from utils import *
 @add_cmd
 def topic(irc, source, msgtarget, args):
     if isAdmin(irc, source):
-        topics = irc.channels[msgtarget]["topic"].split(" | ")
+        topic = irc.channels[msgtarget]["topic"]
+        t = "{} | {}".format(args, topic)
         while True:
-            t = "{} | {}".format(args, irc.channels[msgtarget]["topic"])
             if len(t) > 307:
-                t = t.split(" | ")[0:-1]
+                t = " | ".join(t.split(" | ")[0:-1])
             else:
                 break
         irc.chanmodes[msgtarget].append("TOPIC {} :{}".format(msgtarget, t))
