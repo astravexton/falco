ac85fd7da7f01139e19e120f3075bec09d70f481
nathan <squ1dge08@gmail.com>
2016-02-01 02:08:07 +0000

    to stop abuse with topic command

--- plugins/topic.py
+++ plugins/topic.py
@@ -2,11 +2,13 @@ from utils import *
 
 @add_cmd
 def topic(irc, source, msgtarget, args):
-    topics = irc.channels[msgtarget]["topic"].split(" | ")
-    while True:
-        t = "{} | {}".format(args, irc.channels[msgtarget]["topic"])
-        if len(t) > 307:
-            t = t.split(" | ")[0:-1]
-        else:
-            break
-    irc.send("TOPIC {} :{}".format(msgtarget, t))
+    if isAdmin(irc, source):
+        topics = irc.channels[msgtarget]["topic"].split(" | ")
+        while True:
+            t = "{} | {}".format(args, irc.channels[msgtarget]["topic"])
+            if len(t) > 307:
+                t = t.split(" | ")[0:-1]
+            else:
+                break
+        irc.chanmodes[msgtarget].append("TOPIC {} :{}".format(msgtarget, args))
+        irc.send("PRIVMSG ChanServ :OP {}".format(msgtarget))
