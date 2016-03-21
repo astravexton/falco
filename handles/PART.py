65510dfb6605d9563ad0d1c031972e722d0bce7e
nathan <squ1dge08@gmail.com>
2016-03-21 20:51:50 +0000

    fix PART

--- handles/PART.py
+++ handles/PART.py
@@ -10,7 +10,7 @@ def handle_PART(irc, args):
     host = args.sender.hostmask
 
     if chan not in irc.conf.get("donotlog", []):
-        irc.nicks[nick]["lastaction"] = {"action": "PART", "args": args[1] if len(args) == 2 else "", "time": time.time(), "chan": chan}
+        irc.nicks[nick]["lastaction"] = {"action": "PART", "args": args.args[1] if len(args.args) == 2 else "", "time": time.time(), "chan": chan}
 
     try:
         irc.nicks[nick]["channels"].remove(chan)
