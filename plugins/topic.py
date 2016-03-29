5d63ce1152a7244b9490d3f3596ef5437bce49ce
nathan <squ1dge08@gmail.com>
2016-03-29 21:28:17 +0100

    fix topic not adding blank pipe

--- plugins/topic.py
+++ plugins/topic.py
@@ -2,13 +2,17 @@ from utils import *
 
 @add_cmd
 def topic(irc, source, msgtarget, args):
+    "topic <topic> -- appends <topic> to the current channel topic"
     if isAdmin(irc, source):
-        topic = irc.channels[msgtarget]["topic"]
-        t = "{} | {}".format(args, topic)
-        while True:
-            if len(t) > 307:
-                t = " | ".join(t.split(" | ")[0:-1])
-            else:
-                break
-        irc.chanmodes[msgtarget].append("TOPIC {} :{}".format(msgtarget, t))
-        irc.send("PRIVMSG ChanServ :OP {}".format(msgtarget))
+        if args:
+            topic = irc.channels[msgtarget]["topic"]
+            t = "{} | {}".format(args, topic)
+            while True:
+                if len(t) > 307:
+                    t = " | ".join(t.split(" | ")[0:-1])
+                else:
+                    break
+            irc.chanmodes[msgtarget].append("TOPIC {} :{}".format(msgtarget, t))
+            irc.send("PRIVMSG ChanServ :OP {}".format(msgtarget))
+        else:
+            irc.msg(msgtarget, "topic <topic> -- appends <topic> to the current channel topic")
