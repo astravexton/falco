a4b20023186b81b4a9c69a443caf94aaff3a8c8e
MicroDroid <yousef.su.2000@gmail.com>
2016-04-05 17:14:22 +0300

    Fix multiplication/division in calc and rename to calc.py

--- plugins/calc.py
+++ plugins/calc.py
@@ -4,6 +4,6 @@ form ast import literal_eval
 @add_cmd
 def calc(irc, source, msgtarget, args):
     try:
-        irc.msg(msgtarget, literal_eval(args))
+        irc.msg(msgtarget, eval(args, {}, {}))
     except ValueError:
-        irc.msg(msgtarget, "That's not gonna work")
+        irc.msg(msgtarget, "That's not gonna work");
