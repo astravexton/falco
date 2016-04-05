1c411a87221c7a59c3c55c6b7c6fa0bfb55080c4
MicroDroid <yousef.su.2000@gmail.com>
2016-04-05 18:50:44 +0300

    Rewrite calc.py, to a more secure vesion

--- plugins/calc.py
+++ plugins/calc.py
@@ -1,9 +1,30 @@
 from utils import add_cmd
-form ast import literal_eval
+from math import *
 
+variables = {'__builtins__':None, 'pi':pi}
+functions = {'__builtins__':None, 'acos':acos, 'asin':asin, 'atan':atan, 'atan2':atan2, 'ceil':ceil, 'cos':cos, 'cosh':cosh, 'degrees':degrees,
+'exp':exp, 'fabs':fabs, 'floor':floor, 'fmod':fmod, 'frexp':frexp, 'hypot':hypot, 'ldexp':ldexp, 'log':log, 'log10':log10, 'modf':modf,
+'pow':pow, 'radians':radians, 'sin':sin, 'sinh':sinh, 'sqrt':sqrt, 'tan':tan, 'tanh':tanh}
+
+result = '';
+        
 @add_cmd
 def calc(irc, source, msgtarget, args):
     try:
-        irc.msg(msgtarget, eval(args, {}, {}))
-    except ValueError:
-        irc.msg(msgtarget, "That's not gonna work");
+        set_timeout(math, (args, ), {}, 0.5)
+        irc.msg(msgtarget, result)
+    except (TypeError, ValueError, NameError) as e:
+        irc.msg(msgtarget, "Evaluation error");
+        
+def set_timeout(func, args, kwargs, time):
+    p = Process(target=func, args=args, kwargs=kwargs)
+    p.start()
+    p.join(time)
+    if p.is_alive():
+        p.terminate()
+        return False
+    return True
+    
+def math(expression):
+    result = eval(expression, variables, functions)
+    return result
