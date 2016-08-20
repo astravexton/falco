c8ec2c518d24e06db528969a60e9a68ccafc4e33
nathan <nathan@zyr.io>
2016-08-20 12:32:52 +0100

    fix isAdmin check

--- handles/INVITE.py
+++ handles/INVITE.py
@@ -1,5 +1,5 @@
 import utils
 
 def handle_INVITE(irc, args):
-    if utils.isAdmin(irc, args.sender.hostmask):
+    if utils.isAdmin(irc, args.sender):
         irc.send("JOIN {}".format(args.args[1]))
