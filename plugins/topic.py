ac15ed82061d7651a942b2c3ae1f2d82a75d9a96
nathan <nathan@zyr.io>
2017-01-04 12:26:42 +0000

    fix topic if not set

--- plugins/topic.py
+++ plugins/topic.py
@@ -5,14 +5,17 @@ def topic(irc, source, msgtarget, args):
     "topic <topic> -- appends <topic> to the current channel topic"
     if isAdmin(irc, source):
         if args:
-            topic = irc.channels[msgtarget]["topic"]
-            t = "{} | {}".format(args, topic)
+            topic = irc.channels[msgtarget]["topic"] if "topic" in irc.channels[msgtarget].keys() else ""
+            t = "{} | {}".format(args, topic) if topic else args
             while True:
                 if len(t) > irc.topiclen:
                     t = " | ".join(t.split(" | ")[0:-1])
                 else:
                     break
-            irc.chanmodes[msgtarget].append("TOPIC {} :{}".format(msgtarget, t))
-            irc.send("PRIVMSG ChanServ :OP {}".format(msgtarget))
+            if irc.channels[msgtarget]["nicks"][irc.nick] == "o":
+                irc.send("TOPIC {} :{}".format(msgtarget, t))
+            else:
+                irc.chanmodes[msgtarget].append("TOPIC {} :{}".format(msgtarget, t))
+                irc.send("PRIVMSG ChanServ :OP {}".format(msgtarget))
         else:
             irc.msg(msgtarget, "topic <topic> -- appends <topic> to the current channel topic")
