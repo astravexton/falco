337a073f75b7b1751cfa0ce3e67c1a8fb776c110
nathan <nathan@zyr.io>
2017-01-04 12:27:03 +0000

    fix INVITE to only join autojoin channels in config

--- handles/INVITE.py
+++ handles/INVITE.py
@@ -2,4 +2,5 @@ import utils
 
 def handle_INVITE(irc, args):
     if utils.isAdmin(irc, args.sender):
-        irc.send("JOIN {}".format(args.args[1]))
+        if args.args[1] in irc.autojoin:
+            irc.send("JOIN {}".format(args.args[1]))
