63d1d2895665a0576a788474663a838c327e71ea
Nathan Barley <squ1dge08@gmail.com>
2017-02-02 13:05:05 +0000

    Fix choose

--- plugins/choose.py
+++ plugins/choose.py
@@ -2,7 +2,7 @@ import random
 from utils import add_regex
 
 def choose(irc, target, args, cmdargs):
-    choices = [c.strip() for c in cmdargs.split(",")]
+    choices = [c.strip() for c in cmdargs[0].split(",")]
     if len(choices) > 1 and choices:
         choice = random.choice(choices).strip()
         if choice:
@@ -10,6 +10,6 @@ def choose(irc, target, args, cmdargs):
         elif not choice:
             irc.msg(target, "{}: I can't give you any choice".format(args.sender.nick))
     elif len(choices) == 1:
-        irc.send("PRIVMSG {} :{}: {}".format(target, args.sender.nick, cmdargs))
+        irc.send("PRIVMSG {} :{}: {}".format(target, args.sender.nick, cmdargs[0]))
 
 add_regex(choose, "^\.choose (.*)")
